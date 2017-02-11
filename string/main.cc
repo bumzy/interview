f70713a9d3e0783ffb7c8e1df3fa36770298d837
Bumzy <bumzycm@gmail.com>
2017-02-11 15:06:28 +0800

    implement the main interface of string

--- string/main.cc
+++ string/main.cc
@@ -1,12 +1,161 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 #include "qh_string.h"
 
+//test ctor : string::string()
+void test1()
+{
+    qh::string str1;
+    assert(str1.size() == 0);
+    assert(str1.data() == NULL);
+    assert(str1.c_str() == NULL);
+}
+
+//test ctor : string::string(const char* s)
+void test2()
+{
+    qh::string str1("abc");
+    assert(str1.size() == 3);
+    assert(str1[0] == 'a');
+    assert(str1[1] == 'b');
+    assert(str1[2] == 'c');
+    assert(str1[3] == '\0');
+    assert(!strcmp("abc", str1.data()));
+    assert(!strcmp("abc", str1.c_str()));
+
+    qh::string str2(NULL);
+    assert(str2.size() == 0);
+    assert(str2.data() == NULL);
+    assert(str2.c_str() == NULL);
+
+    qh::string str3("");
+    assert(str3.size() == 0);
+    assert(str3[0] == '\0');
+    assert(!strcmp("", str3.data()));
+    assert(!strcmp("", str3.c_str()));
+}
+
+//test ctor : string::string(const char* s, size_t len)
+void test3()
+{
+    qh::string str1("abc", 5);
+    assert(str1.size() == 3);
+    assert(str1[0] == 'a');
+    assert(str1[1] == 'b');
+    assert(str1[2] == 'c');
+    assert(str1[3] == '\0');
+    assert(!strcmp("abc", str1.data()));
+    assert(!strcmp("abc", str1.c_str()));
+
+    qh::string str2("abc", 3);
+    assert(str2.size() == 3);
+    assert(str2[0] == 'a');
+    assert(str2[1] == 'b');
+    assert(str2[2] == 'c');
+    assert(str2[3] == '\0');
+    assert(!strcmp("abc", str2.data()));
+    assert(!strcmp("abc", str2.c_str()));
+
+    qh::string str3("abc", 2);
+    assert(str3.size() == 2);
+    assert(str3[0] == 'a');
+    assert(str3[1] == 'b');
+    assert(str3[2] == '\0');
+    assert(!strcmp("ab", str3.data()));
+    assert(!strcmp("ab", str3.c_str()));
+
+    qh::string str4("abc", 0);
+    assert(str4.size() == 0);
+    assert(str4[0] == '\0');
+    assert(!strcmp("", str4.data()));
+    assert(!strcmp("", str4.c_str()));
+
+    qh::string str5(NULL, 3);
+    assert(str5.size() == 0);
+    assert(str5.data() == NULL);
+    assert(str5.c_str() == NULL);
+}
+
+//test ctor : string::string(const string& rhs)
+void test4()
+{
+    qh::string str1 = qh::string("abc");
+    assert(str1.size() == 3);
+    assert(str1[0] == 'a');
+    assert(str1[1] == 'b');
+    assert(str1[2] == 'c');
+    assert(str1[3] == '\0');
+    assert(!strcmp("abc", str1.data()));
+    assert(!strcmp("abc", str1.c_str()));
+
+    qh::string str2 = qh::string("");
+    assert(str2.size() == 0);
+    assert(str2[0] == '\0');
+    assert(!strcmp("", str2.data()));
+    assert(!strcmp("", str2.c_str()));
+
+    qh::string str3 = qh::string();
+    assert(str3.size() == 0);
+    assert(str3.data() == NULL);
+    assert(str3.c_str() == NULL);
+}
+
+//test string& string::operator=(const string& rhs)
+void test5()
+{
+    qh::string str1("abc");
+
+    str1 = qh::string();
+    assert(str1.size() == 0);
+    assert(str1.data() == NULL);
+    assert(str1.c_str() == NULL);
+
+    str1 = qh::string("");
+    assert(str1.size() == 0);
+    assert(str1[0] == '\0');
+    assert(!strcmp("", str1.data()));
+    assert(!strcmp("", str1.c_str()));
+
+    qh::string str2 = str1 = qh::string("abc");
+    assert(str2.size() == 3);
+    assert(str2[0] == 'a');
+    assert(str2[1] == 'b');
+    assert(str2[2] == 'c');
+    assert(str2[3] == '\0');
+    assert(!strcmp("abc", str2.data()));
+    assert(!strcmp("abc", str2.c_str()));
+
+    str1 = qh::string(NULL);
+    assert(str1.size() == 0);
+    assert(str1.data() == NULL);
+    assert(str1.c_str() == NULL);
+}
+
+//test string& string::operator=(const string& rhs)
+void test6()
+{
+    qh::string str1("abc");
+    str1[0] = str1[1] = str1[2] = '1';
+    assert(str1[0] == '1');
+    assert(str1[1] == '1');
+    assert(str1[2] == '1');
+    assert(str1[3] == '\0');
+    assert(!strcmp("111", str1.data()));
+    assert(!strcmp("111", str1.c_str()));
+}
 
 int main(int argc, char* argv[])
 {
-    //TODO ÔÚÕâÀïÌí¼Óµ¥Ôª²âÊÔ£¬Ô½¶àÔ½ºÃ£¬´úÂëÂ·¾¶¸²¸ÇÂÊÔ½È«Ô½ºÃ
-    //TODO µ¥Ôª²âÊÔĞ´·¨Çë²Î¿¼INIParserÄÇ¸öÏîÄ¿£¬²»ÒªĞ´Ò»¶Ñprintf£¬ÒªÓÃassert½øĞĞ¶ÏÑÔÅĞ¶Ï¡£
+    //TODO åœ¨è¿™é‡Œæ·»åŠ å•å…ƒæµ‹è¯•ï¼Œè¶Šå¤šè¶Šå¥½ï¼Œä»£ç è·¯å¾„è¦†ç›–ç‡è¶Šå…¨è¶Šå¥½
+    //TODO å•å…ƒæµ‹è¯•å†™æ³•è¯·å‚è€ƒINIParseré‚£ä¸ªé¡¹ç›®ï¼Œä¸è¦å†™ä¸€å †printfï¼Œè¦ç”¨assertè¿›è¡Œæ–­è¨€åˆ¤æ–­ã€‚
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
 
 #ifdef WIN32
     system("pause");
--- string/qh_string.cc
+++ string/qh_string.cc
@@ -1,10 +1,10 @@
 #include "qh_string.h"
-
+#include <assert.h>
 #include <string.h>
 
 namespace qh
 {
-    // TODO 将在这里写实现代码
+    // TODO 陆芦脭脷脮芒脌茂脨麓脢碌脧脰麓煤脗毛
 
     string::string()
         : data_(NULL), len_(0)
@@ -13,25 +13,74 @@ namespace qh
 
     string::string( const char* s )
     {
+        if(s == NULL)
+        {
+            data_ = NULL;
+            len_ = 0;
+        }
+        else
+        {
+            len_ = strlen(s);
+            data_ = new char[len_ + 1];
+            strncpy(data_, s, len_);
+            data_[len_] = '\0';
+        }
     }
 
     string::string( const char* s, size_t len )
     {
+        if(s == NULL)
+        {
+            data_ = NULL;
+            len_ = 0;
+        }
+        else
+        {
+            size_t sLen = strlen(s);
+            len_ = len < sLen ? len : sLen;
+            data_ = new char[len_ + 1];
+            strncpy(data_, s, len_);
+            data_[len_] = '\0';
+        }
     }
 
     string::string( const string& rhs )
     {
-
+        if(rhs.data_ == NULL)
+        {
+            len_ = 0;
+            data_ = NULL;
+        }
+        else
+        {
+            len_ = rhs.len_;
+            data_ = new char[len_+1];
+            strncpy(data_, rhs.data_, len_);
+            data_[len_] = '\0';
+        }
     }
 
     string& string::operator=( const string& rhs )
     {
+        if(this == &rhs)
+            return *this;
+
+        string temp(rhs);
+
+        size_t tLen = len_;
+        len_ = temp.len_;
+        temp.len_ = tLen;
+
+        char * tData = data_;
+        data_ = temp.data_;
+        temp.data_ = tData;
+
         return *this;
     }
 
     string::~string()
     {
-
+        delete[] data_;
     }
 
     size_t string::size() const
@@ -41,16 +90,17 @@ namespace qh
 
     const char* string::data() const
     {
-        return NULL;
+        return data_;
     }
 
     const char* string::c_str() const
     {
-        return NULL;
+        return data_;
     }
 
-    char* string::operator[]( size_t index )
+    char& string::operator[]( size_t index )
     {
-        return NULL;
+        //assert(index < len_);
+        return *(data_ + index);
     }
 }
